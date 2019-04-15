60f03f669350995ba5682580da65536c2da8e4b4
KHAELOM <elkhalomar@gmail.com>
2019-04-15 08:40:33 +0200

    exo4_2

--- exo4.py
+++ exo4.py
@@ -1,8 +1,5 @@
 #Create a program that asks the user for a number and then
-# prints out a list of all the divisors of that number.
-# (If you don’t know what a divisor is, it is a number that
-# divides evenly into another number. For example, 13 is a
-# divisor of 26 because 26 / 13 has no remainder.)
+# prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
 
 
 
@@ -16,6 +13,8 @@ while z < inp :
     k = inp - z
     a = inp % k
     e.append(a)
+
+
     if e[z] == 0 :
        q.append(k)
        z += 1
---
+++ exo4_2.py
@@ -0,0 +1,16 @@
+#Create a program that asks the user for a number and then
+# prints out a list of all the divisors of that number.
+# (If you don’t know what a divisor is, it is a number that
+# divides evenly into another number. For example, 13 is a
+# divisor of 26 because 26 / 13 has no remainder.)
+
+
+num = int(input("number: "))
+a = list(range(1,num+1))
+List = []
+
+for number in a:
+    if num % number == 0:
+        List.append(number)
+
+print(List)
