bd2959cdc0be15e3a8b148d2b7ebc27091eecaef
KHAELOM <elkhalomar@gmail.com>
2019-04-15 17:11:23 +0200

    exo10

--- exo10.py
+++ exo10.py
@@ -4,17 +4,15 @@
 #one list comprehension.
 
 import random
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 a = random.sample(range(100), 10)
 print(a)
-b = random.sample(range(100), 50)
+b = random.sample(range(100), 10)
 print(b)
-c = [x for x in a for z in b if (x == z)]
-d = []
-z = 0
-while z < len(c):
-    if c[z] in d:
-        z += 1
-    else:
-        d.append(c[z])
-print(d)
+c = [x for x in set(a) if x in b]
+
+result = [i for i in c if c.count(i) == 1]
+print(result)
+
 
