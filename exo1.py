86457307535149924525d192cfa152bf73dde774
KHAELOM <elkhalomar@gmail.com>
2019-04-15 04:27:24 +0200

    First commit

---
+++ exo1.py
@@ -0,0 +1,11 @@
+
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+
+name = input("Give me your name: ")
+age =  input("Give me your age: ")
+old = 2119 - int(age)
+print(name + " your " + age + " yo")
+print("you will be 100 yo at ", old)
+
