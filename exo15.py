50ccc4ebacb7bf0958872ee7402dba890215d565
ELKHALOMAR <elkhalomar@gmail.com>
2019-04-17 08:47:29 +0200

    EXO15

--- exo15.py
+++ exo15.py
@@ -1,3 +1,8 @@
+#asks the user for a long string containing multiple words.
+# Print back to the user the same string, except with
+# the words in backwards order.
+
+
 user = input("wright your sentence: ")
 def str_to_list(a):
     inp = a.split(" ")
